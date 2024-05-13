02e4ba9cbcc9f7b3690ef3ba91af8fe135b27641
Jannik Hausladen <jannik.hausladen@fau.de>
2024-05-13 18:15:15 +0200

    [MOD] added martin

--- two_bodies/simulation.py
+++ two_bodies/simulation.py
@@ -10,11 +10,11 @@ class body:
         self.x = np.array(x_0)
         self.v = np.array(v_0)
 
-    def get_dist(self, body):
-        return np.sqrt(sum((self.x - body.x)**2))
+    def get_dist(self, _body):
+        return np.sqrt(sum((self.x - _body.x)**2))
 
-    def get_forces(self, body):
-        return (1 + 1/body.mass) * (body.x - self.x) / self.get_dist(body)**3
+    def get_forces(self, _body):
+        return G * _body.mass * (_body.x - self.x) / self.get_dist(_body)**3
 
     def get_energy(self):
         return 0.5 * (self.mass * sum(self.v ** 2))
@@ -23,9 +23,17 @@ class body:
         self.x = self.x + np.array(x) * tau
         self.v = self.v + np.array(v) * tau
 
+    def martin(self, tau):
+        alpha = mu / np.sqrt(sum(self.x ** 2)) ** 3
+        x_old = self.x
+        v_old = self.v
+        self.x = 1/(1 + tau * alpha) * x_old + tau/(1 + tau * alpha) * v_old
+        self.v = (1 - tau ** 2 * alpha) * v_old - tau * alpha/(1 + tau * alpha) * x_old
+
     def plot(self, fig = None):
         if fig is None:
             plt.plot(self.x[0], self.x[1], 'go')
+            plt.pause(0.01)
 
 class ode_algorithm:
 
@@ -35,19 +43,40 @@ class ode_algorithm:
         self.tau = tau
         self.steps = steps
 
-    def __call__(self, sun, *bodies):
+    def __call__(self, *bodies):
         func = getattr(self, self.approach)
-        func(sun, *bodies)
+        func(*bodies)
 
-    def fwd(self, sun, *bodies):
+    def fwd(self, *bodies):
         N = len(bodies)
         for i in range(self.steps):
-            ret = self.ode(sun, *bodies)
+            ret = self.ode(*bodies)
             ret_x = np.split(ret[:2*N], N)
             ret_v = np.split(ret[2*N:], N)
             ret = np.concatenate([np.concatenate([x, v]) for x,v in zip(ret_x, ret_v)])
-            [body.update(val[:2], val[2:], self.tau) for val, body in zip(np.split(ret, N), bodies)]
-            [body.plot() for body in bodies]
+            [_body.update(val[:2], val[2:], self.tau) for val, _body in zip(np.split(ret, N), bodies)]
+            if not i % 1000:
+                [_body.plot() for _body in bodies]
+
+    def bwd(self, *bodies):
+        N = len(bodies)
+        for i in range(self.steps):
+            ret_old = np.zeros(N*4)
+            for _ in range(20):
+                ret = self.ode(*bodies)
+                ret_x = np.split(ret[:2*N], N)
+                ret_v = np.split(ret[2*N:], N)
+                ret = np.concatenate([np.concatenate([x, v]) for x,v in zip(ret_x, ret_v)])
+                [_body.update(val[:2] - old_val[:2], val[2:] - old_val[2:], self.tau) for val, old_val, _body in zip(np.split(ret, N), np.split(ret_old, N), bodies)]
+                ret_old = ret
+            if not i % 200:
+                [_body.plot() for _body in bodies]
+
+    def martin(self, *bodies):
+        for i in range(self.steps):
+            bodies[1].martin(self.tau)
+            if not i % 100:
+                bodies[1].plot()
 
 class solver:
 
@@ -55,44 +84,49 @@ class solver:
         self.type = _type
         self.tau = tau
 
-    def __call__(self, approach, steps, sun, *bodies):
+    def __call__(self, approach, steps, *bodies):
         if approach == 'fwd':
             ode = self.newton_ode_n
-            fwd_euler = ode_algorithm('fwd', self.tau, steps, ode)
-            return fwd_euler(sun, *bodies)
+        elif approach == 'bwd':
+            ode = self.newton_ode_n
+        elif approach == 'martin':
+            ode = None
+        return ode_algorithm(approach, self.tau, steps, ode)(*bodies)
 
-    def newton_ode(self, sun, body):
-        rhs = np.concatenate([body.x, body.v])
+    def newton_ode(self, *bodies):
+        rhs = np.concatenate([bodies[1].x, bodies[1].v])
         lhs = np.zeros((4,))
         lhs[:2] = rhs[-2:]
 
-        r3 = body.get_dist(sun) ** 3
+        # r3 = bodies[1].get_dist(bodies[0]) ** 3
 
-        mrel = body.mass/sun.mass
-        lhs[-2:] = body.get_forces(sun) # (1+mrel)*rhs[:2]/r3
+        # mrel = body.mass/sun.mass
+        lhs[-2:] = bodies[1].get_forces(bodies[0]) # (1+mrel)*rhs[:2]/r3
 
         return lhs
 
-    def newton_ode_n(self, sun, *bodies):
+    def newton_ode_n(self, *bodies):
         N = len(bodies)
-        rhs = np.concatenate([np.concatenate([body.x for body in bodies]), np.concatenate([body.v for body in bodies])])
+        rhs = np.concatenate([np.concatenate([_body.x for _body in bodies]), np.concatenate([_body.v for _body in bodies])])
         lhs = np.zeros(((N) * 4,))
         lhs[:2*N] = rhs[-2*N:]
 
-        mrel = np.array([body.mass for body in bodies])/sun.mass
+        # mrel = np.array([_body.mass for _body in bodies])/bodies[0].mass
 
         for i in range(0, N):
             fixed_body = bodies[i]
 
-            lhs[2*N + 2*i:2*N + 2*(i+1)] = fixed_body.get_forces(sun) + np.sum([fixed_body.get_forces(body) for body in bodies if body != fixed_body], 0)
+            lhs[2*N + 2*i:2*N + 2*(i+1)] = np.sum([fixed_body.get_forces(_body) for _body in bodies if _body != fixed_body], 0)
 
         return lhs
 
 
 if __name__ == "__main__":
-    earth = body(1, [1, 0], [0, 1])
-    sun = body(333000, [0,0], [0,0])
+    mu = 1.33e20/((1.52e11)**3) * 86400**2
+    G = 6.67e-11/((1.52e11)**3) * 86400**2
+    earth = body(5.976e24, [1 * (1 - 0.0167), 0], [0, np.sqrt((mu * (1.0167))/(1 * (1 - 0.0167)))])
+    sun = body(2e30, [0,0], [0,0])
     jupiter = body(1000, [20,0], [0,0.25])
-    sol = solver('newton', 0.01)
-    ret = sol('fwd', 10000, sun, earth)
+    sol = solver('newton', 0.005)
+    ret = sol('martin', 100000, sun, earth)
     plt.plot(0,0, 'rx')
